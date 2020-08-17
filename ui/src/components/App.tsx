b7ca27c85a74d718aa47b2d4d640a23087b13033
Chris Shepherd <thesheps@gmail.com>
2020-08-17 21:27:53 +0100

    feat(ui): Skeleton

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,3 +1,31 @@
 import React from "react";
+import { Box, Heading, Grommet } from "grommet";
 
-export const App = () => <div>Hey!</div>;
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+export const App = (props) => (
+  <Grommet theme={theme}>
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="light-2"
+      pad={{ vertical: "small", horizontal: "medium" }}
+      elevation="medium"
+      {...props}
+    >
+      <Box direction="row">
+        <Heading margin="none">scratSH</Heading>
+      </Box>
+    </Box>
+  </Grommet>
+);
